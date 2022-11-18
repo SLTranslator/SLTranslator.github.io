8d9723f1e34a6f4b3820b383f3ef9a7e863073fb
David Malek <davidomalek@gmail.com>
2022-11-17 22:20:23 -0500

    added js

--- functionality.js
+++ functionality.js
@@ -3,7 +3,7 @@
 function showType(){document.getElementById("Type").style.display = "";}
 function showRecord(){document.getElementById("Record").style.display = "";}*/
 function showTranslation(){
-  alert("Coming soon!")
+  alert("Currently unavailable. Coming soon!")
   document.getElementById("Output").style.display = "";
 } 
 
